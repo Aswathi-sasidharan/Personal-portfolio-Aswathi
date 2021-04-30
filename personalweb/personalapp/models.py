d98326bb68b4f5d070f2c641036edb6f7b43e7f1
Aswathi-sasidharan <Aswathisasidharan25@gmail.com>
2021-04-30 12:08:14 +0530

    firstcommit

---
+++ personalweb/personalapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+# Create your models here.
+class contact(models.Model):
+    Name=models.CharField(max_length=100)
+    Email= models.CharField(max_length=100)
+    Subject = models.CharField(max_length=100)
+    Message= models.CharField(max_length=100)
+
+
+
+
+
+
+
